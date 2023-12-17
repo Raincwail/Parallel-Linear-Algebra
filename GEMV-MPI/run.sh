59538096eab325b46b3dc79bc2c0b20bced1878a
Korostast <Korostast@gmail.com>
2023-12-17 17:32:40 +0300

    Add results for the first task

--- GEMV-MPI/run.sh
+++ GEMV-MPI/run.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+# Change this list of processes number
+processes=(2 3 4 5 6 7 8)
+
 executable=$1
-processes=(2 3 4 5 6 7 8 9 10 11 12 13 14 15 16)
 log_file="${executable}_log_$2x$3.txt"
 log_file="${log_file//.exe/}"
 times_file="${executable}_times_$2x$3.txt"
 times_file="${times_file//.exe/}"
 
 echo "Run in 1 process..."
-mpiexec -np 1 "$executable" "${times_file}" "$2" "$3" > "${log_file}"
+mpiexec -np 1 "$executable" "$2" "$3" "${times_file}" > "${log_file}"
 
 for proc in "${processes[@]}"; do
   echo "Run in ${proc} processes..."
-  mpiexec -np "$proc" "$executable" "${times_file}" "$2" "$3" >> "${log_file}"
+  mpiexec -np "$proc" "$executable" "$2" "$3" "${times_file}" >> "${log_file}"
 done
